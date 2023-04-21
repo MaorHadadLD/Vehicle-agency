8a89be60458b3734cec79b98d3627300ddeff2f2
Maor Hadad <maorhadad94@gmail.com>
2023-04-21 20:39:05 +0300

    Added a javadoc

--- src/Vehicle/Motorized.java
+++ src/Vehicle/Motorized.java
@@ -1,9 +1,33 @@
 package Vehicle;
 
+/**
+ * The Motorized interface defines the behavior of a motorized vehicle.
+ * It contains methods for setting and getting the average fuel consumption
+ * and the average lifetime of a motorized vehicle.
+ */
 public interface Motorized {
 
+    /**
+     * Sets the average fuel consumption of a motorized vehicle.
+     * @param fuelConsumption the average fuel consumption of the vehicle.
+     */
     public void setAvgFuelConsumption(int fuelConsumption);
+
+    /**
+     * Returns the average fuel consumption of a motorized vehicle.
+     * @return the average fuel consumption of the vehicle.
+     */
     public int getAverageFuelConsumption();
+
+    /**
+     * Sets the average lifetime of a motorized vehicle.
+     * @param avgLifetime the average lifetime of the vehicle.
+     */
     public void setAvgLifetime(int avgLifetime);
+
+    /**
+     * Returns the average lifetime of a motorized vehicle.
+     * @return the average lifetime of the vehicle.
+     */
     public int getAvgLifetime();
 }
