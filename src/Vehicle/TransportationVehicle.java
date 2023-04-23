55a6bd12d5f9166e098378f374b237a0a64053b7
Maor Hadad <maorhadad94@gmail.com>
2023-04-23 17:51:37 +0300

    Added a javadoc

--- src/Vehicle/TransportationVehicle.java
+++ src/Vehicle/TransportationVehicle.java
@@ -2,6 +2,9 @@ package Vehicle;
 
 import java.util.Objects;
 
+/**
+ * An abstract class representing a transportation vehicle.
+ */
 public abstract class TransportationVehicle {
 
     private String model;
@@ -9,7 +12,12 @@ public abstract class TransportationVehicle {
     private int maxPassenger;
     private int maxSpeed;
 
-    //constructor
+    /**
+     * Constructor for TransportationVehicle.
+     * @param model The model of the vehicle.
+     * @param maxPassengers The maximum number of passengers the vehicle can carry.
+     * @param maxSpeed The maximum speed of the vehicle in miles per hour.
+     */
     public TransportationVehicle(String model, int maxPassengers, int maxSpeed) {
         this.model = model;
         this.maxPassenger = maxPassengers;
@@ -17,32 +25,58 @@ public abstract class TransportationVehicle {
         this.distanceTraveled = 0.0;
     }
 
-    //method to update distance traveled
+    /**
+     * Method to update distance traveled.
+     * @param distance The distance to be added to the total distance traveled.
+     */
     public void move(double distance) {
 
         this.distanceTraveled += distance;
     }
 
+    /**
+     * Returns the model of the vehicle.
+     * @return A string representing the model of the vehicle.
+     */
     public String getmodel() {
         return model;
     }
 
+    /**
+     * Returns the total distance traveled by the vehicle.
+     * @return A double representing the total distance traveled in kilometers.
+     */
     public double getDistanceTraveled() {
         return  distanceTraveled;
     }
 
+    /**
+     * Returns the maximum number of passengers the vehicle can carry.
+     * @return An integer representing the maximum number of passengers.
+     */
     public int getMaxPassenger() {
         return maxPassenger;
     }
 
+    /**
+     * Returns the maximum speed of the vehicle.
+     * @return An integer representing the maximum speed in miles per hour.
+     */
     public int getMaxSpeed() {
         return maxSpeed;
     }
 
+    /**
+     * Resets the total distance traveled by the vehicle to zero.
+     */
     public void resetDistanceTraveled() {
         this.distanceTraveled = 0.0;
     }
 
+    /**
+     * Returns a string representation of the vehicle.
+     * @return A string representing the vehicle, including its model, total distance traveled, maximum speed, and maximum number of passengers.
+     */
     public String toString() {
         return getClass().getSimpleName() + ": model: " + model +
                 ", Traveled: " + distanceTraveled + " Km" +
@@ -50,6 +84,11 @@ public abstract class TransportationVehicle {
                 ", Max passengers: " + maxPassenger;
     }
 
+    /**
+     * Compares two TransportationVehicle objects to see if they are equal.
+     * @param o The object to be compared to this one.
+     * @return A boolean indicating whether the objects are equal.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -58,7 +97,10 @@ public abstract class TransportationVehicle {
         return Double.compare(that.distanceTraveled, distanceTraveled) == 0 && maxPassenger == that.maxPassenger && maxSpeed == that.maxSpeed && Objects.equals(model, that.model);
     }
 
-    //abstract methods
+    /**
+     * An abstract method that should be implemented in subclasses to return the type of the vehicle.
+     * @return A string representing the type of the vehicle.
+     */
     public abstract String getType();
 
 }
