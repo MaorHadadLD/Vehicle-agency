ccb2181fae914feac0e61c0251efe501e2ef1e6f
Maor Hadad <maorhadad94@gmail.com>
2023-04-23 17:17:12 +0300

    Added a javadoc

--- src/Vehicle/NonMotorized.java
+++ src/Vehicle/NonMotorized.java
@@ -1,5 +1,9 @@
 package Vehicle;
 
+/**
+ * The EnergyScore enumeration represents the energy efficiency score of a NonMotorized
+ * vehicle, with options A, B, and C.
+ */
 enum EnergyScore {
     A,
     B,
@@ -11,10 +15,33 @@ enum EnergyScore {
     Automaton,
 }*/
 
+/**
+ * The NonMotorized interface represents a vehicle that does not have a motor,
+ * and defines methods to access and modify the power source and energy score of the vehicle.
+ */
 public interface NonMotorized {
 
+    /**
+     * Sets the power source of the vehicle.
+     * @param powerSource a String representing the power source of the vehicle.
+     */
     public void setPowerSource(String powerSource);
+
+    /**
+     * Gets the power source of the vehicle.
+     * @return a String representing the power source of the vehicle.
+     */
     public String getPowerSource();
+
+    /**
+     * Sets the energy efficiency score of the vehicle.
+     * @param energyScore an EnergyScore representing the energy efficiency score of the vehicle.
+     */
     public void setEnergyScore(EnergyScore energyScore);
+
+    /**
+     * Gets the energy efficiency score of the vehicle.
+     * @return an EnergyScore representing the energy efficiency score of the vehicle.
+     */
     public EnergyScore getEnergyScore();
 }
